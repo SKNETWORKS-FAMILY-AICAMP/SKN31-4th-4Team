b32250d0fde2a7545df70047ebb5ae9a45111124
bongrybong <qhdska8402@naver.com>
2026-08-04 19:58:28 +0900

    add

--- bokyakmate/patients/models.py
+++ bokyakmate/patients/models.py
@@ -156,9 +156,6 @@ class Patient(models.Model):
         return self.name
 
 
-# =========================
-# Prescription
-# =========================
 
 class Prescription(models.Model):
     prescription_id = models.CharField(max_length=30, primary_key=True)
